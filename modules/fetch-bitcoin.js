cb05887747946e84f8c1fd482510f319992589cd
greverte <gabriel.reverte@hotmail.com>
2023-04-13 14:36:11 -0300

    arrumando js

---
+++ modules/fetch-bitcoin.js
@@ -0,0 +1,12 @@
+export default function initFetchBitcoin() {
+  fetch('https://www.blockchain.com/pt/ticker')
+    .then(response => response.json())
+    .then(bitcoin => {
+      const btcpreco = document.querySelector('.btc-preco');
+      btcpreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
+    }).catch(erro => {
+      console.log(Error(erro));
+    })
+
+
+}
--- modules/tabnav.js
+++ modules/tabnav.js
@@ -2,7 +2,7 @@ export default function initTabNav() {
   const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
   const tabContent = document.querySelectorAll('[data-tab="content"] section');
 
-  if(tabMenu.length && tabContent.length) {
+  if (tabMenu.length && tabContent.length) {
     tabContent[0].classList.add('ativo');
 
     function activeTab(index) {
